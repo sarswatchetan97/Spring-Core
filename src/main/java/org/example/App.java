b6aaf7033d0b1c31113095e524c40b9581d29ae4
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-03-30 19:31:41 +0530

    Java Based Config

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,19 +13,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1 = context.getBean("alien1", Alien.class);
-//        obj1.setAge(11);
-        System.out.println(obj1.getAge());
-        obj1.code();
-//        Alien obj2 = (Alien) context.getBean("alien1");
-//        System.out.println(obj2.getAge());
-//        obj2.code();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop desktopObj = context.getBean(Desktop.class);
 
-//        Laptop laptopObj = context.getBean(Laptop.class);
-//        laptopObj.machine();
-
-        Desktop obj2 = context.getBean("com2", Desktop.class);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Alien obj1 = context.getBean("alien1", Alien.class);
+////        obj1.setAge(11);
+//        System.out.println(obj1.getAge());
+//        obj1.code();
+////        Alien obj2 = (Alien) context.getBean("alien1");
+////        System.out.println(obj2.getAge());
+////        obj2.code();
+//
+////        Laptop laptopObj = context.getBean(Laptop.class);
+////        laptopObj.machine();
+//
+//        Desktop obj2 = context.getBean("com2", Desktop.class);
 
     }
 }
