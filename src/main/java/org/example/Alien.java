1563242edaac5afeda42030013501493a184a6da
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-03-31 19:03:35 +0530

    @Value @Scope annotation

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+    @Value("20")
     private int age;
 
     //@Autowired
